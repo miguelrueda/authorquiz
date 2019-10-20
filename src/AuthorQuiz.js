021118a298cd59f928f548601f095996f66f51f1
miguel.rueda@outlook.com <$_mrueda91>
2019-10-20 15:12:37 -0500

    State module finished

--- src/AuthorQuiz.js
+++ src/AuthorQuiz.js
@@ -2,18 +2,19 @@ import React from 'react';
 import './App.css';
 import './bootstrap.min.css'
 import PropTypes from 'prop-types'
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 function Hero() {
   return (<div className="row">
     <div className="jumbotron col-10 offset-1">
-    <h1>Author Quiz</h1>
-    <p>Select the book written by the author shown </p>
+      <h1>Author Quiz</h1>
+      <p>Select the book written by the author shown </p>
     </div>
   </div>)
 }
 
-function Turn({author, books, highlight, onAnswerSelected}) {
+function Turn({ author, books, highlight, onAnswerSelected }) {
 
   function highlightToBgColor(highlight) {
     const mapping = {
@@ -25,13 +26,13 @@ function Turn({author, books, highlight, onAnswerSelected}) {
   }
 
   return (
-    <div className="row turn" 
-      style={{backgroundColor: highlightToBgColor(highlight)}}>
+    <div className="row turn"
+      style={{ backgroundColor: highlightToBgColor(highlight) }}>
       <div className="col-4 offset-1">
         <img src={author.imageUrl} className="authorimage" alt="Author" />
       </div>
       <div className="col-6">
-        {books.map((title) => 
+        {books.map((title) =>
           <Book title={title} key={title} onClick={onAnswerSelected} />
         )}
       </div>
@@ -51,7 +52,7 @@ Turn.propTypes = {
   highlight: PropTypes.string.isRequired
 };
 
-function Book({title, onClick}) {
+function Book({ title, onClick }) {
   return (
     <div className="answer" onClick={() => onClick(title)}>
       <h4>{title}</h4>
@@ -59,14 +60,14 @@ function Book({title, onClick}) {
   );
 }
 
-function Continue({show, onContinue}) {
+function Continue({ show, onContinue }) {
   return (
     <div className="row continue">
       {show ?
         <div className="col-11">
           <button className="btn btn-primary btn-lg float-right" onClick={onContinue}>Continue</button>
         </div>
-      : null}
+        : null}
     </div>
   );
 }
@@ -83,17 +84,36 @@ function Footer() {
   );
 }
 
-
-function AuthorQuiz({turnData, highlight, onAnswerSelected, onContinue}) {
-  return (
-    <div className="container-fluid">
-      <Hero />
-      <Turn {...turnData} highlight={highlight} onAnswerSelected={onAnswerSelected} />
-      <Continue show={highlight === 'correct'} onContinue={onContinue} />
-      <p><Link to="/add">Add an author</Link> </p>
-      <Footer />
-    </div>
-  );
+function mapStateToProps(state) {
+  return {
+    turnData: state.turnData,
+    highlight: state.highlight
+  };
 }
+
+function mapDispatchToProps(dispatch) {
+  return {
+    onAnswerSelected: (answer) => {
+      dispatch({ type: 'ANSWER_SELECTED', answer });
+    }, onContinue: () => {
+      dispatch({ type: 'CONTINUE' })
+    }
+  };
+}
+
+const AuthorQuiz = connect(mapStateToProps, mapDispatchToProps)(
+
+  function ({ turnData, highlight, onAnswerSelected, onContinue }) {
+    return (
+      <div className="container-fluid">
+        <Hero />
+        <Turn {...turnData} highlight={highlight} onAnswerSelected={onAnswerSelected} />
+        <Continue show={highlight === 'correct'} onContinue={onContinue} />
+        <p><Link to="/add">Add an author</Link> </p>
+        <Footer />
+      </div>
+    );
+  }
+);
 
 export default AuthorQuiz;
--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import * as serviceWorker from './serviceWorker';
 import { shuffle, sample } from 'underscore';
 import { BrowserRouter, Route, withRouter } from 'react-router-dom';
 import AddAuthorForm from './AddAuthorForm';
+import * as Redux from 'redux';
+import * as ReactRedux from 'react-redux';
 
 const authors = [
   {
@@ -48,14 +50,23 @@ const authors = [
   }
 ];
 
-function resetState() {
-  return {
-    turnData: getTurnData(authors),
-    highlight: ''
-  };
+function reducer(state = { authors, turnData: getTurnData(authors), highlight: '' }, action) {
+  switch (action.type) {
+    case 'ANSWER_SELECTED':
+      console.log(action)
+      const isCorrect = state.turnData.author.books.some((book) => book === action.answer);
+      return Object.assign({}, state, { highlight: isCorrect ? 'correct' : 'wrong' });
+    case 'CONTINUE':
+      return Object.assign({}, state, { highlight: '', turnData: getTurnData(state.authors) });
+    case 'ADD_AUTHOR':
+      return Object.assign({}, state, { authors: state.authors.concat([action.author]) });
+    default:
+      return state;
+  }
 }
 
-let state = resetState();
+let store = Redux.createStore(reducer, 
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 function getTurnData(authors) {
   const allBooks = authors.reduce(function (p, c, i) {
@@ -72,41 +83,16 @@ function getTurnData(authors) {
   }
 }
 
-function onAnswerSelected(answer) {
-  const isCorrect = state.turnData.author.books.some((book) =>
-    book === answer);
-  state.highlight = isCorrect ? 'correct' : 'wrong';
-  render();
-}
-
-function App() {
-  return <AuthorQuiz {...state} 
-    onContinue={() => {
-      state = resetState()
-      render();
-    }}
-    onAnswerSelected={onAnswerSelected} />
-}
-
-const AuthorWrapper = withRouter(({history}) =>
-  <AddAuthorForm onAddAuthor={(author) => {
-    authors.push(author);
-    history.push('/');
-  }} />
-);
-
-function render() {
-  ReactDOM.render(
-    <BrowserRouter>
+ReactDOM.render(
+  <BrowserRouter>
+    <ReactRedux.Provider store={store}>
       <React.Fragment>
-        <Route exact path="/" component={App} />
-        <Route path="/add" component={AuthorWrapper} />
+        <Route exact path="/" component={AuthorQuiz} />
+        <Route path="/add" component={AddAuthorForm} />
       </React.Fragment>
-    </BrowserRouter>,
-    document.getElementById('root'));
-}
-
-render();
+    </ReactRedux.Provider>
+  </BrowserRouter>,
+  document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
